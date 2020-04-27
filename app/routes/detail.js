7dcdae7ea43059e95b0d63dffb8aaffa46a46f3f
Themesama <themesama9@gmail.com>
2020-04-27 16:51:26 +0300

    Last Touch

--- app/routes/detail.js
+++ app/routes/detail.js
@@ -2,6 +2,8 @@ import Route from '@ember/routing/route';
 import fetch from 'fetch';
 import env from '../config/environment';
 import { isArray } from '@ember/array';
+import { action } from '@ember/object';
+import { isBadRequestResponse } from 'ember-fetch/errors';
 
 export default class DetailRoute extends Route {
   host = env.host;
@@ -12,6 +14,8 @@ export default class DetailRoute extends Route {
     return fetch(this.host + "alpha/" + id).then(response => {
       if (response.ok) {
         return response.json();
+      } else if( isBadRequestResponse(response) ) {
+        throw Error(response.status);
       }
     });
   }
@@ -22,7 +26,7 @@ export default class DetailRoute extends Route {
     controller.set('country', model);
 
     // get borders pretty names
-    if( isArray(model.borders) && model.borders.length > 0 ) {
+    if( typeof model !== "undefined" && isArray(model.borders) && model.borders.length > 0 ) {
       fetch(this.host + "alpha?fields=name;alpha2Code&codes=" + model.borders.join(";")).then(response => {
         if (response.ok) {
           response.json().then(borders => {
@@ -30,6 +34,13 @@ export default class DetailRoute extends Route {
           });
         }
       });
+    }
+  }
+
+  @action
+  error(error, transition) {
+    if( error ) {
+      this.transitionTo('index');
     }
   }
 }
